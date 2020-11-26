57ce2dfe10fd07f9ce6fefde5351bd311d2a993c
Giulia Straccali <straccaligiulia@gmail.com>
2020-11-26 14:58:43 +0100

    Creazione del codice Unicode

--- Unicode/Program.cs
+++ Unicode/Program.cs
@@ -6,7 +6,14 @@ namespace Unicode
     {
         static void Main(string[] args)
         {
-           
+            string unicodeString =
+            	 "This unicode string has 2 characters outside the" +
+           	     " ASCII range: \n" +
+            	 "Pi (\u03A0), and Sigma (\u03A3)."; 
+            Console.WriteLine("Original String");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicodeString);
+            Console.WriteLine();
         }
     }
 }
