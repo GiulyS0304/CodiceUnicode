8500bb37c0bb2babcedede4eeb3b3f466e87569c
Giulia Straccali <straccaligiulia@gmail.com>
2020-11-26 15:03:03 +0100

    Inserimento della frase a console

--- Unicode/Program.cs
+++ Unicode/Program.cs
@@ -6,10 +6,13 @@ namespace Unicode
     {
         static void Main(string[] args)
         {
-            string unicodeString =
-            	 "This unicode string has 2 characters outside the" +
-           	     " ASCII range: \n" +
-            	 "Pi (\u03A0), and Sigma (\u03A3)."; 
+            //string unicodeString =
+            //	 "This unicode string has 2 characters outside the" +
+            //     " ASCII range: \n" +
+            //	 "Pi (\u03A0), and Sigma (\u03A3)."; 
+            string unicodeString = "";
+            Console.WriteLine("Inserisci una frase"); // Dico all'utente di inserire una frase da console
+            unicodeString = Console.ReadLine(); // Permetto all'utente di inserire una frase a console
             Console.WriteLine("Original String");
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodeString);
