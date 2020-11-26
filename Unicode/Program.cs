1f4bd008a72b105cdc7b33f7f372186472fdefc3
Giulia Straccali <straccaligiulia@gmail.com>
2020-11-26 15:19:47 +0100

    Creazione ramo "sviluppo"

--- Unicode/Program.cs
+++ Unicode/Program.cs
@@ -17,6 +17,22 @@ namespace Unicode
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodeString);
             Console.WriteLine();
+
+            // ESEMPIO CHE CONVERTE UN VALORE NUMERICO O ALFANUMERICO IN CODICE ASCII
+
+            //Console.WriteLine("Inserisci un valore numerico o alfanumerico da convertire in codice ASCII");
+            //char carattere = Convert.ToChar(Console.ReadLine());
+
+            //int number = carattere;
+            //// aggiunge 4 al numero decimale corrispondente della lettera inserita
+            //// number = number + 4;
+
+            //Console.WriteLine($"Il carattere digitato nella tabella ASCII cosrrisponde al numero: {number.ToString()}");
+            //string binario = Convert.ToString(number, 2).PadLeft(8, '0');
+
+            //Console.WriteLine($"Il carattere in codice binario corrisponde alla sequenza di bit {binario}");
+            //Console.ReadKey();
+
         }
     }
 }
